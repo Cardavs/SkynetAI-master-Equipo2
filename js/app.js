8f32c61bcf9437808bad7b188cee2bde7247343f
Cardavs <92052582+Cardavs@users.noreply.github.com>
2023-03-08 20:07:45 -0600

    Plantilla

---
+++ js/app.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".sidenav");
+  var instances = M.Sidenav.init(elems);
+  var elems = document.querySelectorAll(".tooltipped");
+  var instances = M.Tooltip.init(elems);
+  var elems = document.querySelectorAll(".collapsible");
+  var instances = M.Collapsible.init(elems, {
+    accordion: false,
+  });
+  var elems = document.querySelectorAll(".parallax");
+  var instances = M.Parallax.init(elems);
+});
